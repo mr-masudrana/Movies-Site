746cd0416b6cf297f59c007fa669b8bc213adbb8
Masud Rana <71147447+mr-masudrana@users.noreply.github.com>
2025-04-23 22:35:44 +0600

    Update script.js

--- admin/script.js
+++ admin/script.js
@@ -1,29 +1,29 @@
 document.getElementById('uploadMovies').addEventListener('submit', function (e) {
-      e.preventDefault();
+  e.preventDefault();
 
-      const title = document.getElementById('title').value.trim();
-      const year = document.getElementById('year').value.trim();
-      const category = document.getElementById('category').value.trim();
-      const posterurl = document.getElementById('posterurl').value.trim();
-      const driveid = document.getElementById('driveid').value.trim();
+  const title = document.getElementById('title').value.trim();
+  const year = document.getElementById('year').value.trim();
+  const category = document.getElementById('category').value.trim();
+  const posterurl = document.getElementById('posterurl').value.trim();
+  const driveid = document.getElementById('driveid').value.trim();
 
-      const data = {
-        title,
-        year,
-        category,
-        posterurl,
-        driveid
-      };
+  const data = {
+    title,
+    year,
+    category,
+    posterurl,
+    driveid
+  };
 
-      fetch('https://script.google.com/macros/s/AKfycbxPjYT60yT9iOskWpMod8nt7QuoTbWL3kT3VP4jajxz3honsTLg29Isaj9noif4L3yi/exec', {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
+  fetch('https://script.google.com/macros/s/AKfycbxPjYT60yT9iOskWpMod8nt7QuoTbWL3kT3VP4jajxz3honsTLg29Isaj9noif4L3yi/exec', {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
 
-      alert('Movie uploaded successfully!');
-      document.getElementById('uploadMovies').reset();
-    });
+  alert('Movie uploaded successfully!');
+  document.getElementById('uploadMovies').reset();
+});
